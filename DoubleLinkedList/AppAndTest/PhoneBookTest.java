0ace1357dee82b1e275949030501fc710518d09c
iscript3r <163893499+iscript3r@users.noreply.github.com>
2024-09-14 15:25:42 +0300

    Update PhoneBookTest.java

--- DoubleLinkedList/AppAndTest/PhoneBookTest.java
+++ DoubleLinkedList/AppAndTest/PhoneBookTest.java
@@ -1,20 +1,5 @@
-/****************************
-CLASS: PhoneBookTest.java
-CSC212 Data structures - Project phase I
-Fall 2023
 
-EDIT DATE:
-10-16-2023
 
-TEAM:
-Fahad AlJadaan's Group . 
-
-AUTHORS:
-Fahad Adel AlJadaan (443105685)
-Nawaf Mohammed AlSaeed (443102065)
-Mishari Khalid AlBuhairi (443102188)
-
-***********************************/
 
 public class PhoneBookTest {
 
