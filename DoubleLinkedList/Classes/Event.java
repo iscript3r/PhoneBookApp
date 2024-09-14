65e392b73b7879964c2fa9c13b6bcafd6235f107
iscript3r <163893499+iscript3r@users.noreply.github.com>
2024-09-14 15:26:24 +0300

    Event.java

--- DoubleLinkedList/Classes/Event.java
+++ DoubleLinkedList/Classes/Event.java
@@ -1,20 +1,3 @@
-/****************************
-CLASS: Event.java
-CSC212 Data structures - Project phase I
-Fall 2023
-
-EDIT DATE:
-10-16-2023
-
-TEAM:
-Fahad AlJadaan's Group . 
-
-AUTHORS:
-Fahad Adel AlJadaan (443105685)
-Nawaf Mohammed AlSaeed (443102065)
-Mishari Khalid AlBuhairi (443102188)
-
-***********************************/
 
 public class Event implements Comparable<String> {
 	private String title;
